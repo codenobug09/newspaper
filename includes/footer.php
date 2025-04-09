cb9f2a8a736a6e87e9fbab5f46aad7d5786d7697
Chaun09 <bangcac24@gmail.com>
2025-04-09 14:18:13 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    <footer class="py-5" style="background-color: red;">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Tap Chi Doanh Nghiep Tiep Thi <?php echo date('Y');?>
+            </p>
+        </div>
+        <!-- /.container -->
+    </footer>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark fixed-top" style="background-color: red;">
+     <div class="container" style="background-color: red;">
+         <a class="navbar-brand" href="index.php"><img src="images/Logo_TC_DNTT10052022.png" height="50"></a>
+         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+             data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+             aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarResponsive">
+             <ul class="navbar-nav ml-auto">
+                 <li class="nav-item">
+                     <a class="nav-link" style="color: black;" href="index.php">Home</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" ` style="color: black;" href="about-us.php">About</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" style="color: black;" href="index.php">News</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" style="color: black;" href="contact-us.php">Contact us</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" style="color: black;" href="admin/">Admin</a>
+                 </li>
+
+             </ul>
+         </div>
+     </div>
+ </nav>
